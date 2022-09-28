28f41f7d7efbf5038b6da06ca8b2584278cfa784
Isaias Lipa <isaiaslipa@isaiass-mbp.ads.northwestern.edu>
2022-09-28 00:46:36 -0500

    initial schedule page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const schedule = {
+  title: 'CS Courses 2022-2023'
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          {schedule.title}
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
